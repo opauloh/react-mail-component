3349428347e36f208aaee422637e11e5f5e2e59d
opauloh <paulo.scape@gmail.com>
2020-10-04 19:57:35 -0300

    apply composition pattern to Card to add more flexibility

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,35 +1,21 @@
 import React from 'react';
-import { MjmlColumn, MjmlText } from 'mjml-react';
+import { MjmlColumn, MjmlDivider, MjmlHero, MjmlSection, MjmlText } from 'mjml-react';
 
-const Card = ({ title, description, action }) => (
-  <MjmlColumn width="48%" backgroundColor="#e5e5e5" borderRadius="8px" cssClass="card">
-    <MjmlText
-      paddingTop="2rem"
-      color="#000000"
-      fontWeight="900"
-      fontFamily="Roboto, Arial, sans-serif"
-      fontSize="3rem"
-      lineHeight="3.2rem">
-      {title}
-    </MjmlText>
-    <MjmlText
-      paddingTop="0.75rem"
-      color="#000000"
-      fontFamily="Roboto, Arial, sans-serif"
-      fontSize="1rem"
-      lineHeight="1.2rem">
-      {description}
-    </MjmlText>
-    {action && (
-      <MjmlText
-        paddingBottom="1.5rem"
-        fontSize="0.875rem"
-        fontFamily="Roboto Mono, Arial, sans-serif">
-        <a href={action.href} className="link-orange">
-          {action.name}
-        </a>
-      </MjmlText>
-    )}
+const Card = ({ children, ...props }) => <MjmlColumn {...props}>{children}</MjmlColumn>;
+
+Card.Title = ({ children, ...props }) => <MjmlText {...props}>{children}</MjmlText>;
+
+Card.Description = ({ children, ...props }) => <MjmlText {...props}>{children}</MjmlText>;
+
+Card.Action = ({ href, children, ...props }) => (
+  <MjmlText {...props}>
+    <a href={href}>{children}</a>
+  </MjmlText>
+);
+
+Card.Cover = ({ children, ...props }) => (
+  <MjmlColumn width="100%" {...props}>
+    {children}
   </MjmlColumn>
 );
 
