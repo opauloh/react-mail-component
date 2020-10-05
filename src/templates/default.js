445029622c7b3b7bad2a08ffba837c0d0794165c
opauloh <paulo.scape@gmail.com>
2020-10-05 11:55:02 -0300

    update default theme with new decoupled components

--- src/templates/default.js
+++ src/templates/default.js
@@ -7,10 +7,16 @@ import Header from '../components/Header';
 import Container from '../components/Container';
 import Card from '../components/Card';
 
-export const generate = ({ title, to_name, show_day, description, cards, action }) => {
+const pallete = {
+  primary: '#20639B',
+  text: '#ffffff'
+};
+export const generate = ({ title, description, cards, action }) => {
   return (
     <Container title={title}>
-      <Header title={title} name={to_name} showDay={show_day} />
+      <Header>
+        <Header.Title>{title}</Header.Title>
+      </Header>
       <MjmlSection backgroundColor="#ffffff">
         <MjmlColumn>
           {description &&
@@ -36,39 +42,74 @@ export const generate = ({ title, to_name, show_day, description, cards, action 
             <React.Fragment key={index}>
               <Card
                 type={card.type}
-                width= '48%'
-                backgroundColor= '#e5e5e5'
-                borderRadius= '8px'
-                cssClass= 'card'
-              >
+                width="48%"
+                backgroundColor="#e5e5e5"
+                borderRadius="8px"
+                cssClass="card">
                 <Card.Title
-                  paddingTop= '2rem'
-                  color= '#000000'
-                  fontWeight= '900'
-                  fontFamily= 'Roboto, Arial, sans-serif'
-                  fontSize= '3rem'
-                  lineHeight= '3.2rem'
-                >{card.title}</Card.Title>
+                  paddingTop="2rem"
+                  color="#000000"
+                  fontWeight="900"
+                  fontFamily="Roboto, Arial, sans-serif"
+                  fontSize="3rem"
+                  lineHeight="3.2rem">
+                  {card.title}
+                </Card.Title>
                 <Card.Description
-                  paddingTop= '0.75rem'
-                  color= '#000000'
-                  fontFamily= 'Roboto, Arial, sans-serif'
-                  fontSize= '1rem'
-                  lineHeight= '1.2rem'
-                >{card.description}</Card.Description>
+                  paddingTop="0.75rem"
+                  color="#000000"
+                  fontFamily="Roboto, Arial, sans-serif"
+                  fontSize="1rem"
+                  lineHeight="1.2rem">
+                  {card.description}
+                </Card.Description>
                 <Card.Action
                   href={card.action.href}
-                  paddingBottom= '1.5rem'
-                  fontSize= '0.875rem'
-                  fontFamily= 'Roboto Mono, Arial, sans-serif'
-                >{card.action.name}</Card.Action>
+                  paddingBottom="1.5rem"
+                  fontSize="0.875rem"
+                  fontFamily="Roboto Mono, Arial, sans-serif">
+                  {card.action.name}
+                </Card.Action>
               </Card>
               {index % 2 === 0 && <MjmlColumn width="4%"> </MjmlColumn>}
             </React.Fragment>
           ))}
       </MjmlSection>
-      {action && <Button action={action} />}
-      <Footer />
+      {action && (
+        <MjmlSection>
+          <MjmlColumn width="100%">
+            {action.map((btn, idx) => {
+              switch (btn.color) {
+                case 'secondary':
+                  return (
+                    <Button
+                      border={`1px solid ${pallete.primary}`}
+                      backgroundColor={pallete.text}
+                      color={pallete.primary}
+                      key={idx}
+                      href={btn.href}>
+                      {btn.name}
+                    </Button>
+                  );
+                default:
+                  return (
+                    <Button
+                      color={pallete.text}
+                      borderRadius="4px"
+                      backgroundColor={pallete.primary}
+                      key={idx}
+                      href={btn.href}>
+                      {btn.name}
+                    </Button>
+                  );
+              }
+            })}
+          </MjmlColumn>
+        </MjmlSection>
+      )}
+      <Footer>
+        <Footer.Text>Footer</Footer.Text>
+      </Footer>
     </Container>
   );
 };
